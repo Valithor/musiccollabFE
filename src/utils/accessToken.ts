e1c4b10d24351b5c04eba45fc90ff2f22c8a8e6f
Valithor <valithor9@gmail.com>
2021-06-22 20:08:35 +0200

    Heroku

--- src/utils/accessToken.ts
+++ src/utils/accessToken.ts
@@ -1,7 +1,14 @@
+import Cookies from 'universal-cookie';
+
 let accessToken = "";
 
 export const setAccessToken = (s: string) => {
+  const cookies = new Cookies();
   accessToken = s;
+  console.log(accessToken)
+  console.log(accessToken!=="")
+  cookies.set('jid', accessToken, {path: '/refresh_token', httpOnly: true});
+  
 };
 
 export const getAccessToken = () => {
